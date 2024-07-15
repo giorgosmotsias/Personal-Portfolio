07b0317601d886573b20068399e0d1f6f123a41b
User <User@DESKTOP-AR2C95P.local>
2024-07-15 21:15:35 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,14 @@ import React  from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div className='content'>
             <h1>Home</h1>
             <p>Hi, I'm Giorgos Motsias, a software engineer with expertise in developing innovative solutions.
                 Welcome to my personal portfolio where you can explore my projects, skills, and professional experience.
             </p>
         </div>
 
-    )
-}
+    );
+};
 
 export default Home;
