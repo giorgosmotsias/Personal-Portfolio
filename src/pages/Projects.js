1e9947350eab7b0f1f35ab5e9fcc292659e7da04
giorgosmotsias <giorgosmotsias@outlook.com>
2024-07-15 22:01:11 +0300

    Update Projects.js

---
+++ src/pages/Projects.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Projects = () => {
+    return (
+        <div className="content">
+            <h1>My Projects</h1>
+
+            <ul>
+                <li>
+                    <a href="https://github.com/giorgosmotsias/TimeTabling">TimeTabling</a>
+                </li>
+                <li>
+                    <a href="https://github.com/giorgosmotsias/calcappTest">Calculator</a>
+                </li>
+                <li>
+                    <a href="https://github.com/giorgosmotsias/Personal-Portfolio">React Portfolio</a>
+                </li>
+                
+            </ul>
+
+        </div>
+    );
+};
+
+export default Projects;
