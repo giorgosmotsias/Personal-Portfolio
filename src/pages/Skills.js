d7d91f7755da9da53848db6d4c7c9b57b8b6b3ec
giorgosmotsias <giorgosmotsias@outlook.com>
2024-07-24 21:56:45 +0300

    Update Skills.css and Skills.js

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHtml5, faCss3Alt, faJsSquare, faReact, faNode, faGithub} from "@fortawesome/free-brands-svg-icons";
+import { faDatabase, faServer } from "@fortawesome/free-solid-svg-icons";
+import "./Skills.css";
 
-const Skills = () => {
-    return (
-        <div>
-        <h1>Skills</h1> 
-
-        </div>
-    )
-}   
+const skills = [
+    { icon: faHtml5, name: "HTML" },
+    { icon: faCss3Alt, name: "CSS" },
+    { icon: faJsSquare, name: "JavaScript" },
+    { icon: faServer, name: "Express.js" },
+    { icon: faReact, name: "React.js" },
+    { icon: faNode, name: "Node.js" },
+    { icon: faDatabase, name: "MongoDB" },
+    { icon: faGithub, name: "GitHub" },
+  ];
+  
+    const Skills = () => {
+        return (
+          <div className="content">
+            <h1>Technologies I've Worked With</h1>
+            <div className="skills-grid">
+              {skills.map((skill, index) => (
+                <div className="skill" key={index}>
+                  <FontAwesomeIcon icon={skill.icon} size="3x" />
+                  <p>{skill.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        );
+      };
+      
 
 export default Skills;
