995b4021a7bf6811d4a1b3c05a5997fd20b9f0ef
giorgosmotsias <giorgosmotsias@outlook.com>
2024-07-29 00:17:35 +0300

    Update .gitignore, package-lock.json, and 2 more files...

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+
+const app = express();
+const port = 3000;
