9db83b0fd832fdb5c0940b45b7eacf9eb24a5fab
giorgosmotsias <giorgosmotsias@outlook.com>
2024-09-07 00:55:52 +0300

    Update Header.css, Header.js, and index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,36 +3,44 @@ import { Link } from 'react-scroll';
 import './Header.css';
 
 const Header = () => {
-
   const [ShowHeader, setShowHeader] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
-      if(window.scrollY > lastScrollY) {
-        // scroll down
+      if (window.scrollY > lastScrollY) {
         setShowHeader(false);
       } else {
-        // scroll up
         setShowHeader(true);
       }
-      // remember current page's y position in the scroll
       setLastScrollY(window.scrollY);
     };
-    // Add the event listener for the scroll event
     window.addEventListener('scroll', handleScroll);
-    return() => window.removeEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, [lastScrollY]);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
     <nav className={`navbar ${ShowHeader ? 'visible' : 'hidden'}`}>
-      <ul>
-        {/* <li><Link to="home" smooth={true} duration={1000}>Home</Link></li> */}
-        <li><Link to="about" smooth={true} duration={1000}>About</Link></li>
-        <li><Link to="projects" smooth={true} duration={1000}>Projects</Link></li>
-        <li><Link to="skills" smooth={true} duration={1000}>Skills</Link></li>
-        <li><Link to="experience" smooth={true} duration={1000}>Experience</Link></li>
-        <li><Link to="contact" smooth={true} duration={1000}>Contact</Link></li>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <ul className={`menu ${isOpen ? 'open' : ''}`}>
+        <li><Link to="about" smooth={true} duration={1000} onClick={closeMenu}>About</Link></li>
+        <li><Link to="projects" smooth={true} duration={1000} onClick={closeMenu}>Projects</Link></li>
+        <li><Link to="skills" smooth={true} duration={1000} onClick={closeMenu}>Skills</Link></li>
+        <li><Link to="experience" smooth={true} duration={1000} onClick={closeMenu}>Experience</Link></li>
+        <li><Link to="contact" smooth={true} duration={1000} onClick={closeMenu}>Contact</Link></li>
       </ul>
     </nav>
   );
